f1a53f5e72fcdf90ac10afe07de5e7dbbe5d39d9
Myagmar Lam <miigaalam@gmail.com>
2023-09-12 22:02:40 -0500

    flash added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,5 +5,8 @@ import * as jquery from "jquery"
 import "semantic-ui"
 
 $(document).on('turbo:load', function() {
-    $('.ui.dropdown').dropdown()
+    $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function() {
+      $(this).closest('.message').transition('fade');
+    });
 });
